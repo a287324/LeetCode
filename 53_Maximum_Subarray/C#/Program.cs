eb56e3ce79c1d2f7a62b553ec60bb79dd2540431
a287324 <a287324@gmail.com>
2023-09-02 21:35:02 +0800

    create 53 C#

---
+++ 53_Maximum_Subarray/C#/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution solution = new Solution();
+            int[] num = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            //int[] num = new int[] { 5, 4, -1, 7, 8 };
+
+            Console.WriteLine("" + solution.MaxSubArray(num));
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int res = int.MinValue;
+            int sum = 0;
+            foreach(var item in nums)
+            {
+                sum = Math.Max(sum + item, item);
+                if(res < sum)
+                {
+                    res = sum;
+                }
+            }
+            return res;
+        }
+    }
+}
+
+
+
+
