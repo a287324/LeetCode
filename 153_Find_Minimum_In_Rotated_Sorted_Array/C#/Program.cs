b1810f26f89a4d85d0de8c114679e50daa4a398c
a287324 <a287324@gmail.com>
2023-10-16 22:54:36 +0800

    create 153_Find_Minimum_In_Rotated_Sorted_Array C#

---
+++ 153_Find_Minimum_In_Rotated_Sorted_Array/C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            return nums.Min();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            int[] nums = { 3, 4, 5, 1, 2 };
+            Console.WriteLine(sol.FindMin(nums));
+        }
+    }
+
+}
