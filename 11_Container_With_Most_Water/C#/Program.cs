d9608bcccd691a050b492421796ded172546237a
a287324 <a287324@gmail.com>
2023-10-16 22:44:10 +0800

    create 11_Container_With_Most_Water C#

---
+++ 11_Container_With_Most_Water/C#/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int areaNow, areaMax = 0;
+            while (left < right)
+            {
+                areaNow = Math.Min(height[left], height[right]) * (right - left);
+                areaMax = Math.Max(areaNow, areaMax);
+                if (height[left] < height[right])
+                {
+                    left++;
+                } 
+                else
+                {
+                    right--;
+                }
+            }
+            return areaMax;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Console.WriteLine(sol.MaxArea(height));
+        }
+    }
+
+}
