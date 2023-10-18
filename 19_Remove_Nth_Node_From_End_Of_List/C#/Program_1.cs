ddf40b0fa822c1ccf4fc5481de76d22e4163fea1
a287324 <a287324@gmail.com>
2023-10-18 14:38:46 +0800

    create 19_Remove_Nth_Node_From_End_Of_List C#

---
+++ 19_Remove_Nth_Node_From_End_Of_List/C#/Program_1.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            int listLen = 0;
+            // 首先尋訪到底一次，確認 List 的總長
+            ListNode reg = head;
+            while (reg != null)
+            {
+                reg = reg.next;
+                listLen++;
+            }
+            // 刪除 head 開頭指向的節點
+            if(listLen == n)
+            {
+                head = head.next;
+                return head;
+            }
+            // 先到要刪除的指定節點的前一個節點
+            reg = head;
+            for(int i = 0; i < (listLen - n - 1); i++)
+            {
+                reg = reg.next;
+            }
+            reg.next = reg.next.next;
+            return head;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            // 初始化 (1,2,3,4,5)
+            //ListNode list = new ListNode(1);
+            //ListNode head = list;
+            //for (int i = 2; i <= 5; i++)
+            //{
+            //    head.next = new ListNode(i);
+            //    head = head.next;
+            //}
+
+            // 初始化 (1)
+            //ListNode list = new ListNode(1);
+            //ListNode head = list;
+
+            // 初始化 (1,2)
+            ListNode list = new ListNode(1);
+            list.next = new ListNode(2);
+            ListNode head;
+            // 
+            head = sol.RemoveNthFromEnd(list, 2);
+            while(head != null )
+            {
+                Console.WriteLine(head.val);
+                head = head.next;
+            }
+        }
+    }
+
+}
---
+++ 19_Remove_Nth_Node_From_End_Of_List/C#/Program_2.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            // 在 head 前面多加一個空節點(多空節點的目的是如果要刪除的是 head 目前節點，會需要特別處理)
+            ListNode newHead = new ListNode();
+            newHead.next = head;
+
+            // 根據 n 移動右邊界，讓左邊界與右邊的間距剛好是 n
+            ListNode left = newHead;
+            ListNode right = head;
+            for(int i = 0; i < n; i++)
+            {
+                right = right.next;
+            }
+            // 保持左右邊界的距離下，移動右邊界，這樣要刪除的節點剛好是左邊界的下個節點
+            while(right != null)
+            {
+                right = right.next;
+                left = left.next;
+            }
+            left.next = left.next.next;
+            return newHead.next;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            // 初始化 (1,2,3,4,5)
+            //ListNode list = new ListNode(1);
+            //ListNode head = list;
+            //for (int i = 2; i <= 5; i++)
+            //{
+            //    head.next = new ListNode(i);
+            //    head = head.next;
+            //}
+
+            // 初始化 (1)
+            //ListNode list = new ListNode(1);
+            //ListNode head = list;
+
+            // 初始化 (1,2)
+            ListNode list = new ListNode(1);
+            list.next = new ListNode(2);
+            ListNode head;
+            // 
+            head = sol.RemoveNthFromEnd(list, 2);
+            while(head != null )
+            {
+                Console.WriteLine(head.val);
+                head = head.next;
+            }
+        }
+    }
+
+}
