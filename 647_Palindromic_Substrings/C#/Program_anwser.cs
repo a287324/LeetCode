71aa6b3a7a2dbd077020823d6f3bacc9ad647bc4
a287324 <a287324@gmail.com>
2023-10-18 17:46:33 +0800

    create 647_Palindromic_Substrings C#

---
+++ 647_Palindromic_Substrings/C#/Program_anwser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int CountSubstrings(string s)
+        {
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                CountPalindroms(s, i, i, ref count);
+                CountPalindroms(s, i, i + 1, ref count);
+            }
+
+            return count;
+        }
+
+        private void CountPalindroms(string s, int leftIndex, int rightIndex, ref int count)
+        {
+            while (leftIndex >= 0 && rightIndex < s.Length && s[leftIndex] == s[rightIndex])
+            {
+                leftIndex--;
+                rightIndex++;
+                count++;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            string str = "abbac";
+            Console.WriteLine(sol.CountSubstrings(str));
+        }
+    }
+
+}
---
+++ 647_Palindromic_Substrings/C#/Program_myself.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int CountSubstrings(string s)
+        {
+            int count = 0;
+            int len = s.Length;
+
+            char[] charsInv = s.ToCharArray();
+            Array.Reverse(charsInv);
+            string sInv = new string(charsInv);
+
+            count += len;   // 字元數為 1 可省略，因為每個字都一定是 Palindromic
+            for (int w = 2; w <= len; w++)   // 控制字元數
+            {
+                for(int i = 0; i < (len - w + 1); i++) // 控制起始值
+                {
+                    string segmentStr = s.Substring(i, w);
+                    string segmentStrInv = sInv.Substring((len - i - w), w);
+                    if (segmentStr == segmentStrInv)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            string str = "abbac";
+            Console.WriteLine(sol.CountSubstrings(str));
+        }
+    }
+
+}
