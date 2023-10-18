3f4c54c332add3d4529392b53e9baec7f63e4c1f
a287324 <a287324@gmail.com>
2023-10-18 14:21:36 +0800

    modify 206_Reverse_Linked_List C#

--- 206_Reverse_Linked_List/C#/Program_recursion.cs
+++ 206_Reverse_Linked_List/C#/Program_recursion.cs
@@ -1,11 +1,4 @@
-﻿// 參考資料 : https://youtu.be/BLUj14tLhv8?t=7109&si=h6CkIC3_kUTpW9Hr
-// 方法的概念 :
-// 1. 透過 現在極值(maxNow / minNow) 來決定 subarray 的起點
-// 2. 下個極值(maxNext / minNext) 協助 現在極值(maxNow / minNow) 來判斷是否更換起點值
-// 3. 歷史極值(max)記錄 現在極大值(maxNow) 的歷史最大值
-
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.ConstrainedExecution;
